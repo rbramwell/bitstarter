298e5f0211041eb477295dfab8f8974586f8c300
Rory Bramwell <rory.bramwell@gmail.com>
2013-07-10 14:22:30 -0500

    Updated web.js to read from and display contents of index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var msgBuf = fs.readFileSync("index.html");
+  var msgStr = msgBuf.toString();
+  response.send(msgStr);
 });
 
 var port = process.env.PORT || 5000;
