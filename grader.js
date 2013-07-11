e274e3ebbac758af916c44854a511c8a862178b0
Rory Bramwell <rory.bramwell@gmail.com>
2013-07-10 19:08:10 -0500

    Added functionality to grader.js to accept URLs as input

--- grader.js
+++ grader.js
@@ -22,6 +22,8 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
+var util = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -33,6 +35,11 @@ var assertFileExists = function(infile) {
 	console.log("%s does not exist. Exiting.", instr);
 	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+    return instr;
+};
+
+var assertURLExists = function(inURL) {
+    var instr = inURL.toString();
     return instr;
 };
 
@@ -48,11 +55,35 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
+
     for(var ii in checks) {
 	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
     }
-    return out;
+
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+};
+
+var checkWebpageURL = function(urltowebpage, checksfile) {
+    rest.get(urltowebpage).on('complete', function(result, response) {
+	if (result instanceof Error) {
+    console.error('Error: ' + util.format(response.message));
+	}
+	else {
+    $ = cheerio.load(result);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+
+    for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+    }
+
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+	}
+    });
 };
 
 var clone = function(fn) {
@@ -65,10 +96,15 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_to_webpage>', 'URL to webpage', clone(assertURLExists))
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if(program.url) {
+	checkWebpageURL(program.url, program.checks);
+    }
+    else {
+	checkHtmlFile(program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
